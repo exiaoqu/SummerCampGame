89d1e1a2709a20afddcfc69ac4035f21d6a6bd94
eweilzh <weiliangzhang@ericsson.com>
2017-07-24 15:53:36 +0800

    add the flowers and particle system

---
+++ Assets/Scripts/EnergyPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyPickup : MonoBehaviour {
+
+	//PlayInventory playInventory;
+
+	void Start(){
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			gameObject.SetActive (false);
+		}
+	}
+
+}
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour 
+{
+	public int collectedEnergy = 0;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
