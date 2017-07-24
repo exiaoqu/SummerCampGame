b696aa2eb93876030c97d5d2f9bcaf9093cb5c95
Xiaoyu.Qu <xiaoyu.qu@ericsson.com>
2017-07-24 15:46:26 +0800

    Add enter mushroom door switch scene effect

---
+++ Assets/EnterMushroomDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterMushroomDoor : MonoBehaviour {
+    public GameObject enterMushroomSpark;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter Mushroom door!");
+
+        // initiate EnterMushroomSpark
+        Instantiate(enterMushroomSpark, other.transform.position, other.transform.rotation);  
+    }
+}
---
+++ Assets/Scripts/DestoryByTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestoryByTime : MonoBehaviour {
+    public float lifeTime;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnDestroy()
+    {
+        // switch scene
+        SceneManager.LoadScene("FantasticForest", LoadSceneMode.Single);
+    }
+}
