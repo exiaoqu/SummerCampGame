1b435b4daafeaf31fb3bf5a0868ffe73f73d05ce
eweilzh <weiliangzhang@ericsson.com>
2017-07-27 15:17:16 +0800

    Fix Bug: talk with npc and add the flowerSingle

--- Assets/kittenMeet.cs
+++ Assets/kittenMeet.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class kittenMeet : MonoBehaviour {
-	public float meetDistance = 0.5f;
+	public float meetDistance = 0.25f;
 
 	private static string CAT = "Player";
 	PlayerInventory playerInventory;
 	AudioSource[] audioList;
-	private bool speakFlag = true;
 	private Animation kitAnimation;
 	private GameObject player;
 	private Vector3 npcPosition;
@@ -28,7 +27,6 @@ public class kittenMeet : MonoBehaviour {
 		//Debug.Log ("npc z = "+ npcPosition.z );
 		float x = player.transform.localPosition.x - npcPosition.x;
 		float z = player.transform.localPosition.z - npcPosition.z;
-		speakFlag = true;
 
 		return ((x * x + z*z) < meetDistance * (playerInventory.playerScale/2.0f)) ? true : false;
 	}
@@ -50,36 +48,39 @@ public class kittenMeet : MonoBehaviour {
 
 	void meetAction(){
 		//GetComponent<Animation>().Play("Ithcing");
-		if (isMeet ()) {
-			
-			if (speakFlag) {
-				
-				audioAction();
-				speakFlag = false;
-
-				/*
-				for (int i = 0; i < 19; i++) {
-					Debug.Log ("meet");
-					GetComponent<Animation>().Play("Ithcing");
-					kitAnimation.Play("Jump");
-					kitAnimation.Play("Walk");
-				}
-				*/
-			}
-
-
+		if (isMeet ()) {			
+			audioAction();
 		}
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		meetAction ();
+		//0 left, 1 right, 2 middle button
+		//if (Input.GetMouseButtonDown (2)) {
+		if(Input.GetKeyDown(KeyCode.T)) {
+			Debug.Log ("KeypadEnter 2");
+			meetAction ();
+		}
+
 			
 	}
 
 
+
+
+
 	/*
+	 * 
+	void OnCollisionEnter(Collision collision) {
+		
+		Debug.Log ("OnCollisionEnter ");
+		foreach (ContactPoint contact in collision.contacts) {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        if (collision.relativeVelocity.magnitude > 2)
+			audioList [0].Play ();
+	}
 	void OnTriggerEnter(Collider other) {
 		if (other.tag.Equals (CAT)) {
 			audioList [0].Play ();
